31e93b7b8a8bf68e3ddb0724f9646d201b7e1d23
codethazine <dario.finocchiaro01@gmail.com>
2023-07-22 16:49:52 +0200

    Merge and save everything together

--- eval_script_historical.py
+++ eval_script_historical.py
@@ -12,118 +12,109 @@ GPT_35_MAP = {'openaichat/gpt-3.5-turbo-0301':'March 2023',"openaichat/gpt-3.5-t
 
 ################# SOLVING MATH PROBLEMS #################
 # PRIME Dataset
-prime_data = pandas.read_csv(open('generation/PRIME_EVAL.csv'))
-prime_metric = 'Accuracy'
+mathsolve_data = pandas.read_csv(open('generation/PRIME_EVAL.csv'))
+mathsolve_metric = 'Accuracy'
 
 # Accuracy 
-prime_acc_score, prime_acc_scores_std = MyAnalysis.get_score(prime_data, prime_metric)
-print(prime_acc_score)
+mathsolve_acc_score, mathsolve_acc_scores_std = MyAnalysis.get_score(mathsolve_data, mathsolve_metric)
 
 # Verbosity
-prime_verb_score, prime_verb_scores_std = MyAnalysis.get_verbosity(prime_data)
-print(prime_verb_score)
+mathsolve_verb_score, mathsolve_verb_scores_std = MyAnalysis.get_verbosity(mathsolve_data)
 
-print("\n")
-# Overlap
-prime_overlap_score_gpt4, prime_overlap_scores_std_gpt4 = MyAnalysis.get_overlap(data=prime_data,models=models_GPT4,name=prime_metric)
-print(prime_overlap_score_gpt4)
-prime_overlap_score_gpt35, prime_overlap_scores_std_gpt35 = MyAnalysis.get_overlap(data=prime_data,models=models_GPT35,name=prime_metric)
-print(prime_overlap_score_gpt35)
+# Overlap # SKIP OVERLAP AND CALCULATE IT ON CLIENT SIDE
+'''
+mathsolve_overlap_score_gpt4, mathsolve_overlap_scores_std_gpt4 = MyAnalysis.get_overlap(data=mathsolve_data,models=models_GPT4,name=mathsolve_metric)
+mathsolve_overlap_score_gpt35, mathsolve_overlap_scores_std_gpt35 = MyAnalysis.get_overlap(data=mathsolve_data,models=models_GPT35,name=mathsolve_metric)
+'''
 
 # Normalize data into a single dataframe
-# Columns are: model, timestamp, accuracy, verbosity # SKIP OVERLAP AND CALCULATE IT ON CLIENT SIDE
-prime_df = pandas.DataFrame(columns=['model','timestamp','accuracy','verbosity'])
-prime_df['model'] = ['openaichat/gpt-3.5-turbo-0301','openaichat/gpt-3.5-turbo-0613', 'openaichat/gpt-4-0314','openaichat/gpt-4-0613']
-prime_df['timestamp'] = ['2023-03-01','2023-06-13','2023-03-01','2023-06-14']
-prime_df['accuracy'] = [prime_acc_score['openaichat/gpt-3.5-turbo-0301'],prime_acc_score['openaichat/gpt-3.5-turbo-0613'],prime_acc_score['openaichat/gpt-4-0314'],prime_acc_score['openaichat/gpt-4-0613']]
-prime_df['verbosity'] = [prime_verb_score['openaichat/gpt-3.5-turbo-0301'],prime_verb_score['openaichat/gpt-3.5-turbo-0613'],prime_verb_score['openaichat/gpt-4-0314'],prime_verb_score['openaichat/gpt-4-0613']]
+# Columns are: model, timestamp, accuracy, verbosity
+mathsolve_df = pandas.DataFrame(columns=['model','timestamp','mathsolve_accuracy','mathsolve_verbosity'])
+mathsolve_df['model'] = ['openaichat/gpt-3.5-turbo-0301','openaichat/gpt-3.5-turbo-0613', 'openaichat/gpt-4-0314','openaichat/gpt-4-0613']
+mathsolve_df['timestamp'] = ['2023-03-01','2023-06-13','2023-03-01','2023-06-14']
+mathsolve_df['mathsolve_accuracy'] = [mathsolve_acc_score['openaichat/gpt-3.5-turbo-0301'],mathsolve_acc_score['openaichat/gpt-3.5-turbo-0613'],mathsolve_acc_score['openaichat/gpt-4-0314'],mathsolve_acc_score['openaichat/gpt-4-0613']]
+mathsolve_df['mathsolve_verbosity'] = [mathsolve_verb_score['openaichat/gpt-3.5-turbo-0301'],mathsolve_verb_score['openaichat/gpt-3.5-turbo-0613'],mathsolve_verb_score['openaichat/gpt-4-0314'],mathsolve_verb_score['openaichat/gpt-4-0613']]
 
-print(prime_df)
-
-
-'''
 ################# CODE GENERATION #################
 # LEETCODE Dataset
-data = pandas.read_csv(open('generation/LEETCODE_EASY_EVAL.csv'))
-name = "leetcode"
+codegen_data = pandas.read_csv(open('generation/LEETCODE_EASY_EVAL.csv'))
+codegen_metric = 'Directly Executable'
 
 # Directly Executable
-metric = 'Directly Executable'
-score, scores_std = MyAnalysis.get_score(data,metric)
-MyVisual.plot_bars(score,scores_std,fontsize=fontsize,name_map=GPT_4_MAP)
-MyVisual.save_figure(name+"_GPT4.svg")
-MyVisual.plot_bars(score,scores_std,fontsize=fontsize,name_map=GPT_35_MAP)
-MyVisual.save_figure(name+"_GPT35.svg")
+codegen_acc_score, codegen_acc_scores_std = MyAnalysis.get_score(codegen_data, codegen_metric)
 
 # Verbosity
-score, scores_std = MyAnalysis.get_verbosity(data)
-MyVisual.plot_bars(score,scores_std,yrange=[0,600],fontsize=fontsize,name_map=GPT_4_MAP,no_text=True)
-MyVisual.save_figure(name+"_verbose_GPT4.svg")
-MyVisual.plot_bars(score,scores_std,yrange=[0,800],fontsize=fontsize,name_map=GPT_35_MAP,no_text=True)
-MyVisual.save_figure(name+"_verbose_GPT35.svg")
+codegen_verb_score, codegen_verb_scores_std = MyAnalysis.get_verbosity(codegen_data)
 
-# Overlap
-score, scores_std = MyAnalysis.get_overlap(data=data,models=models_GPT4,name=metric)
-MyVisual.plot_bar(score,scores_std,fontsize=fontsize)
-MyVisual.save_figure(name+"_overlap_GPT4.svg")
-score, scores_std = MyAnalysis.get_overlap(data=data,models=models_GPT35,name=metric)
-MyVisual.plot_bar(score,scores_std,fontsize=fontsize)
-MyVisual.save_figure(name+"_overlap_GPT35.svg")
+# Overlap # SKIP OVERLAP AND CALCULATE IT ON CLIENT SIDE
+'''
+codegen_overlap_score_gpt4, codegen_overlap_scores_std_gpt4 = MyAnalysis.get_overlap(data=codegen_data,models=models_GPT4,name=codegen_metric)
+codegen_overlap_score_gpt35, codegen_overlap_scores_std_gpt35 = MyAnalysis.get_overlap(data=codegen_data,models=models_GPT35,name=codegen_metric)
+'''
 
+# Normalize data into a single dataframe
+# Columns are: model, timestamp, accuracy, verbosity 
+codegen_df = pandas.DataFrame(columns=['model','timestamp','codegen_accuracy','codegen_verbosity'])
+codegen_df['model'] = ['openaichat/gpt-3.5-turbo-0301','openaichat/gpt-3.5-turbo-0613', 'openaichat/gpt-4-0314','openaichat/gpt-4-0613']
+codegen_df['timestamp'] = ['2023-03-01','2023-06-13','2023-03-01','2023-06-14']
+codegen_df['codegen_accuracy'] = [codegen_acc_score['openaichat/gpt-3.5-turbo-0301'],codegen_acc_score['openaichat/gpt-3.5-turbo-0613'],codegen_acc_score['openaichat/gpt-4-0314'],codegen_acc_score['openaichat/gpt-4-0613']]
+codegen_df['codegen_verbosity'] = [codegen_verb_score['openaichat/gpt-3.5-turbo-0301'],codegen_verb_score['openaichat/gpt-3.5-turbo-0613'],codegen_verb_score['openaichat/gpt-4-0314'],codegen_verb_score['openaichat/gpt-4-0613']]
 
 ################# VISUAL REASONING #################
 # ARC Dataset
-data = pandas.read_csv(open('generation/ARC_EVAL.csv'))
-name = 'arc'
+vizreason_data = pandas.read_csv(open('generation/ARC_EVAL.csv'))
+vizreason_metric = 'Exact Match'
 
-# Directly Executable
-metric = 'Exact Match'
-score, scores_std = MyAnalysis.get_score(data,metric)
-MyVisual.plot_bars(score,scores_std,fontsize=fontsize,name_map=GPT_4_MAP)
-MyVisual.save_figure(name+"_GPT4.svg")
-MyVisual.plot_bars(score,scores_std,fontsize=fontsize,name_map=GPT_35_MAP)
-MyVisual.save_figure(name+"_GPT35.svg")
+# Exact Match
+vizreason_acc_score, vizreason_acc_scores_std = MyAnalysis.get_score(vizreason_data, vizreason_metric)
 
 # Verbosity
-score, scores_std = MyAnalysis.get_verbosity(data)
-MyVisual.plot_bars(score,scores_std,yrange=[0,400],fontsize=fontsize,name_map=GPT_4_MAP,no_text=True)
-MyVisual.save_figure(name+"_verbose_GPT4.svg")
-MyVisual.plot_bars(score,scores_std,yrange=[0,400],fontsize=fontsize,name_map=GPT_35_MAP,no_text=True)
-MyVisual.save_figure(name+"_verbose_GPT35.svg")
+vizreason_verb_score, vizreason_verb_scores_std = MyAnalysis.get_verbosity(vizreason_data)
 
-# Overlap
-score, scores_std = MyAnalysis.get_overlap(data=data,models=models_GPT4,name=metric)
-MyVisual.plot_bar(score,scores_std,fontsize=fontsize)
-MyVisual.save_figure(name+"_overlap_GPT4.svg")
-score, scores_std = MyAnalysis.get_overlap(data=data,models=models_GPT35,name=metric)
-MyVisual.plot_bar(score,scores_std,fontsize=fontsize)
-MyVisual.save_figure(name+"_overlap_GPT35.svg")
+# Overlap # SKIP OVERLAP AND CALCULATE IT ON CLIENT SIDE
+'''
+vizreason_overlap_score_gpt4, vizreason_overlap_scores_std_gpt4 = MyAnalysis.get_overlap(data=vizreason_data,models=models_GPT4,name=vizreason_metric)
+vizreason_overlap_score_gpt35, vizreason_overlap_scores_std_gpt35 = MyAnalysis.get_overlap(data=vizreason_data,models=models_GPT35,name=vizreason_metric)
+'''
+
+# Normalize data into a single dataframe
+# Columns are: model, timestamp, accuracy, verbosity
+vizreason_df = pandas.DataFrame(columns=['model','timestamp','vizreason_accuracy','vizreason_verbosity'])
+vizreason_df['model'] = ['openaichat/gpt-3.5-turbo-0301','openaichat/gpt-3.5-turbo-0613', 'openaichat/gpt-4-0314','openaichat/gpt-4-0613']
+vizreason_df['timestamp'] = ['2023-03-01','2023-06-13','2023-03-01','2023-06-14']
+vizreason_df['vizreason_accuracy'] = [vizreason_acc_score['openaichat/gpt-3.5-turbo-0301'],vizreason_acc_score['openaichat/gpt-3.5-turbo-0613'],vizreason_acc_score['openaichat/gpt-4-0314'],vizreason_acc_score['openaichat/gpt-4-0613']]
+vizreason_df['vizreason_verbosity'] = [vizreason_verb_score['openaichat/gpt-3.5-turbo-0301'],vizreason_verb_score['openaichat/gpt-3.5-turbo-0613'],vizreason_verb_score['openaichat/gpt-4-0314'],vizreason_verb_score['openaichat/gpt-4-0613']]
 
 ################# ANSWERING SENSITIVE QUESTIONS #################
 # SensitiveQ Dataset
-data = pandas.read_csv(open('generation/SENSITIVEQ_EVAL.csv'))
-name = 'sensitiveq'
+sensitiveq_data = pandas.read_csv(open('generation/SENSITIVEQ_EVAL.csv'))
+sensitiveq_metric = 'Answer Rate'
 
-# Directly Executable
-metric = 'Answer Rate'
-score, scores_std = MyAnalysis.get_score(data,metric)
-MyVisual.plot_bars(score,scores_std,fontsize=fontsize,name_map=GPT_4_MAP)
-MyVisual.save_figure(name+"_GPT4.svg")
-MyVisual.plot_bars(score,scores_std,fontsize=fontsize,name_map=GPT_35_MAP)
-MyVisual.save_figure(name+"_GPT35.svg")
+# Answer Rate
+sensitiveq_acc_score, sensitiveq_acc_scores_std = MyAnalysis.get_score(sensitiveq_data, sensitiveq_metric)
 
 # Verbosity
-score, scores_std = MyAnalysis.get_verbosity(data)
-MyVisual.plot_bars(score,scores_std,yrange=[0,800],fontsize=fontsize,name_map=GPT_4_MAP,no_text=True)
-MyVisual.save_figure(name+"_verbose_GPT4.svg")
-MyVisual.plot_bars(score,scores_std,yrange=[0,500],fontsize=fontsize,name_map=GPT_35_MAP,no_text=True)
-MyVisual.save_figure(name+"_verbose_GPT35.svg")
+sensitiveq_verb_score, sensitiveq_verb_scores_std = MyAnalysis.get_verbosity(sensitiveq_data)
 
-# Overlap
-score, scores_std = MyAnalysis.get_overlap(data=data,models=models_GPT4,name=metric)
-MyVisual.plot_bar(score,scores_std,fontsize=fontsize)
-MyVisual.save_figure(name+"_overlap_GPT4.svg")
-score, scores_std = MyAnalysis.get_overlap(data=data,models=models_GPT35,name=metric)
-MyVisual.plot_bar(score,scores_std,fontsize=fontsize)
-MyVisual.save_figure(name+"_overlap_GPT35.svg")
+# Overlap # SKIP OVERLAP AND CALCULATE IT ON CLIENT SIDE
 '''
+sensitiveq_overlap_score_gpt4, sensitiveq_overlap_scores_std_gpt4 = MyAnalysis.get_overlap(data=sensitiveq_data,models=models_GPT4,name=sensitiveq_metric)
+sensitiveq_overlap_score_gpt35, sensitiveq_overlap_scores_std_gpt35 = MyAnalysis.get_overlap(data=sensitiveq_data,models=models_GPT35,name=sensitiveq_metric)
+'''
+
+# Normalize data into a single dataframe
+# Columns are: model, timestamp, accuracy, verbosity
+sensitiveq_df = pandas.DataFrame(columns=['model','timestamp','sensitiveq_accuracy','sensitiveq_verbosity'])
+sensitiveq_df['model'] = ['openaichat/gpt-3.5-turbo-0301','openaichat/gpt-3.5-turbo-0613', 'openaichat/gpt-4-0314','openaichat/gpt-4-0613']
+sensitiveq_df['timestamp'] = ['2023-03-01','2023-06-13','2023-03-01','2023-06-14']
+sensitiveq_df['sensitiveq_accuracy'] = [sensitiveq_acc_score['openaichat/gpt-3.5-turbo-0301'],sensitiveq_acc_score['openaichat/gpt-3.5-turbo-0613'],sensitiveq_acc_score['openaichat/gpt-4-0314'],sensitiveq_acc_score['openaichat/gpt-4-0613']]
+sensitiveq_df['sensitiveq_verbosity'] = [sensitiveq_verb_score['openaichat/gpt-3.5-turbo-0301'],sensitiveq_verb_score['openaichat/gpt-3.5-turbo-0613'],sensitiveq_verb_score['openaichat/gpt-4-0314'],sensitiveq_verb_score['openaichat/gpt-4-0613']]
+
+################# TOTAL #################
+# Join all the dataframes on model/timestamp
+total_df = pandas.merge(mathsolve_df, codegen_df, on=['model','timestamp'])
+total_df = pandas.merge(total_df, vizreason_df, on=['model','timestamp'])
+total_df = pandas.merge(total_df, sensitiveq_df, on=['model','timestamp'])
+
+# Save to CSV
+total_df.to_csv('output/total_hist_eval.csv', index=False)
